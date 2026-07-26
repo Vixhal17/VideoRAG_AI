e46eba7e9defbd3fc2692b8d5b1369bfea5456a1
Vishal Chauhan <vishal170305@gmail.com>
2026-07-26 16:14:01 +0530

    Implement RAG chain functionality

---
+++ core/rag_engine.py
@@ -0,0 +1,105 @@
+import os
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough , RunnableLambda
+from core.vector_Store import build_vector_store,load_vector_store,get_retriever
+
+def get_llm():
+  return ChatMistralAI(
+    model = 'mistral-small-latest',
+    mistral_api_key = os.getenv("MISTRAL_API_KEY"),
+    temperature=0.3
+  )
+
+def format_docs(docs):
+  return "\n\n\n\n".join([doc.page_content for doc in docs])
+
+
+def build_rag_chain(transcript : str):
+
+  vector_store = build_vector_store(transcript)
+
+  retriever = get_retriever(vector_store , k = 4)
+
+  llm = get_llm()
+
+  prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system","""
+              You are an AI Video Assistant.
+              Answer the user's question using ONLY the provided transcript context.
+              If the answer cannot be found in the transcript, reply:
+              "I couldn't find that information in the video."
+              Be concise and accurate.
+              Transcript Context : {context}
+              """
+        ),
+        ("human", "{question}")
+    ]
+  )
+
+  # full LCEL Rag Pipeline
+
+  rag_chain = (
+     {
+      "context" : retriever | RunnableLambda(format_docs),
+      "question" : RunnablePassthrough()
+      }
+      |prompt | llm | StrOutputParser()
+  )
+
+  return rag_chain
+
+
+
+def load_rag_chain():
+
+    vector_store = load_vector_store()
+
+    retriever = get_retriever(vector_store, k=4)
+
+    llm = get_llm()
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system",
+                """
+                  You are an AI Video Assistant.
+                  Answer the user's question using ONLY the provided transcript context.
+                  If the answer is not present in the transcript, reply:
+                  "I couldn't find that information in the video."
+                  Context : {context}
+                  """
+            ),
+            ("human", "{question}")
+        ]
+    )
+
+    rag_chain = (
+        {
+            "context": retriever | RunnableLambda(format_docs),
+            "question": RunnablePassthrough(),
+        }
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return rag_chain
+
+
+
+def ask_question(rag_chain, question: str) -> str:
+    try:
+        print(f"Question: {question}")
+
+        answer = rag_chain.invoke(question)
+
+        print(f"Answer: {answer}")
+
+        return answer
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return "An error occurred while processing your question."
